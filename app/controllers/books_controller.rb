b3825ceb29beb3810af4630f744bdaba249d70e2
Javier <javier.su.weijie@gmail.com>
2014-11-14 17:13:50 +0800

    adds edit and new actions on books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,39 @@
 class BooksController < ApplicationController
+  http_basic_authenticate_with name: "admin", password: "admin", except: [:index, :show]
+
+  before_filter :load_book, except: [:index, :new, :create]
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.create book_params
+    render :show
+  end
+
   def index
     @books = Book.all
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    @book.update_attributes book_params
+    render :show
+  end
+
+  private
+
+  def load_book
     @book = Book.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :price, :isbn10, :isbn13, :copies)
   end
 end
