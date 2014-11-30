961f4d85db19f6a6658a59c5cdc3a0176cbbb91a
Swayam <swayam.narain@gmail.com>
2014-12-01 00:02:51 +0800

    removes unncessary scope form line items

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,7 +1,6 @@
 class LineItem < ActiveRecord::Base
   belongs_to :book
   belongs_to :order
-  scope :from_this_month, -> { where(:created_at => Time.now.beginning_of_month..Time.now.end_of_month) }
 
   def authors_list
     authors.map {|author| "#{author.full_name}"}.join(',')
