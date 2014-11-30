d8d86759cad1b6c5a45b109291e675458f16e798
Swayam <swayam.narain@gmail.com>
2014-11-30 22:40:47 +0800

    adds method for getting top m books of the month on book

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,39 @@ class Book < ActiveRecord::Base
   has_and_belongs_to_many :authors
   belongs_to :publisher
   has_many :opinions
+  attr_accessor :count
+
+  def self.hash_to_book(book_hash)
+    book = Book.new() 
+    attrs = book_hash.reject{|k,v| !book.attributes.keys.member?(k.to_s) and k != "count" }
+    book.attributes = attrs
+    return book
+  end
+
+  def self.best_sellers_this_month(m)
+    result = Book.connection.execute("
+      SELECT books.* , SUM(quantity) as count
+      FROM line_items JOIN books JOIN orders
+      WHERE line_items.created_at BETWEEN '#{Time.now.beginning_of_month}' AND '#{Time.now.end_of_month}'
+      AND orders.status = 1
+
+      UNION
+
+      SELECT *, 0 as count
+      FROM books
+
+      GROUP BY books.id
+      ORDER BY count DESC
+      LIMIT #{m}
+      ")
+
+    books = []
+    result.each do |book_hash|
+      books << Book.hash_to_book(book_hash)
+    end
+    return books
+  end
+
 
   def authors_list
     authors.map {|author| "#{author.full_name}"}.join(',')
@@ -9,6 +42,20 @@ class Book < ActiveRecord::Base
 
   def publisher_name
     try(:publisher).try(:name)
+  end
+
+  def copies_sold_this_month(m)
+    result = ActiveRecord::Base.connection.execute("
+      SELECT SUM(quantity) as count
+      FROM line_items JOIN orders
+      WHERE book_id = #{id}
+      AND orders.status = 1
+      AND line_items.created_at BETWEEN '#{Time.now.beginning_of_month}' AND '#{Time.now.end_of_month}'
+      ORDER BY count DESC
+      LIMIT #{m}
+    ")
+    return result[0]["count"] if result.any? 
+    return 0
   end
 
   def useful_opinions(number: 5)
